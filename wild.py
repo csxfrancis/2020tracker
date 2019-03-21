ddd2ac20471182790051fb78855ffb30112a4159
csxfrancis <36423310+csxfrancis@users.noreply.github.com>
2019-03-21 14:11:34 -0500

    Add files via upload

---
+++ wild.py
@@ -0,0 +1,48 @@
+import tweepy
+count = 0
+f = open("tweets.txt",'a')
+EW = 0
+BO = 0
+DemTwitterHandles = {'Beto O\'Roarke' : '@betoorourke','Elizabeth Warren' : "ewarren",'Bernie Sanders' : 'BernieSanders'}
+
+DemCandidates = {'Beto O\'Roarke' : 0 ,'Elizabeth Warren': 0,'Joe Biden' : 0, 'Bernie Sanders' : 0}
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        for name in DemCandidates.keys():
+            if name in status.text:
+                DemCandidates[name] +=1
+
+
+        try:
+            print(status.text)
+            f.write(status.text)
+
+
+
+            print(status.text)
+        except:
+            print("can't print character")
+        print(DemCandidates)
+
+consid = 'd5P1uyUG97sIfpSpNrGeSnnZH'
+
+secret = 'lfNQCvqvpLLFLlZ3V0uGA9FitfnCrp5E0f834OcKfHVi5yYyAc'
+actk = '1041405628655263754-x8G3lxMFfPG2hXQzVGZjipjqSVGOjN'
+sctk = 'PRtGHfHtpAvIdPemLpc03xrVpgjTBqYsWraoO0XoG0Bi1'
+auth = tweepy.OAuthHandler(consid,secret)
+auth.set_access_token(actk,sctk)
+api = tweepy.API(auth)
+
+lner = MyStreamListener()
+lner = tweepy.Stream(auth = api.auth, listener=lner)
+lner.filter(track=DemCandidates)
+
+"""
+tweets = tweepy.Cursor(api.search,q="//1").items(100)
+for page in tweepy.Cursor(api.search,q ='@realDonaldTrump',count=10).pages(10):
+    for tweet in page:
+        r = tweet.text
+        if(len(r)<100):
+            print(r)
+"""
